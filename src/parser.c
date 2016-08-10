5615914e4b68e5a7d791522f8538a6359a3703b6
c650 <cbailey@palmertrinity.org>
2016-08-09 20:47:50 -0400

    changes to packet parser

--- src/parser.c
+++ src/parser.c
@@ -1,9 +1,3 @@
-/*
-:oaktree!~purple@unaffiliated/oaktree347 PRIVMSG ##0x00sec
- :it will make it HELLA easier to parse
-*/
-
-
 int parse_irc_packet(char* buf, IRCPacket* packet) {
 
 	if (strstr(buf, "!") == NULL || strstr(buf, "@") == NULL) {
@@ -17,24 +11,28 @@ int parse_irc_packet(char* buf, IRCPacket* packet) {
 	printf("[*] Analyzing %s\n", buf);
 
 	packet->sender = strtok(buf, "!")+1; // gets user between : and !
-	printf("[+] packet->sender = %s\n", packet->sender);
 
 	packet->realname = strtok(NULL, "@")+1; // skip over ~
-	printf("[+] packet->realname = %s\n", packet->realname);
 
 	packet->hostname = strtok(NULL, " ");
-	printf("[+] packet->hostname = %s\n", packet->hostname);
 
 	packet->type = strtok(NULL, " ");
-	printf("[+] packet->type = %s\n", packet->type);
-
+	
 	packet->channel = strtok(NULL, " ");
-	printf("[+] packet->channel = %s\n", packet->channel);
 
 	packet->content = strtok(NULL, "\0");
-	if (packet->content != NULL && !strcmp(packet->type, "PRIVMSG"))
+
+	if ( packet->content != NULL && !strcmp(packet->type, "PRIVMSG") )
 		packet->content++;
-	
+
+	if (!strcmp(packet->type, "NICK"))
+		packet->channel++; // skip : in NICK messages
+
+	printf("[+] packet->sender = %s\n", packet->sender);
+	printf("[+] packet->realname = %s\n", packet->realname);
+	printf("[+] packet->hostname = %s\n", packet->hostname);
+	printf("[+] packet->type = %s\n", packet->type);
+	printf("[+] packet->channel = %s\n", packet->channel);
 	printf("[+] packet->content = %s\n", packet->content);
 
 	return 1;
